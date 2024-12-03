bb836e2de382b961727c507a943c5851037214ae
Sebastian Ross <sebbeross@gmail.com>
2024-12-03 20:47:01 +0100

    Tidy

--- day3.js
+++ day3.js
@@ -2,15 +2,18 @@ import { readFileSync } from "node:fs";
 
 const line = readFileSync("./day3.txt", { encoding: "UTF8" });
 
-const matches = [...line.matchAll(/mul\((\d+),(\d+)\)/g)];
-const result = matches.reduce((acc, match) => {
-  acc += parseInt(match[1]) * parseInt(match[2]);
-  return acc;
-}, 0);
+const result = [...line.matchAll(/mul\((\d+),(\d+)\)/g)].reduce(
+  (acc, match) => {
+    acc += parseInt(match[1]) * parseInt(match[2]);
+    return acc;
+  },
+  0
+);
 
-const matches2 = [...line.matchAll(/mul\((\d+),(\d+)\)|don't\(\)|do\(\)/g)];
 let flag = true;
-const result2 = matches2.reduce((acc, match) => {
+const result2 = [
+  ...line.matchAll(/mul\((\d+),(\d+)\)|don't\(\)|do\(\)/g),
+].reduce((acc, match) => {
   if (match[0] === "don't()") {
     flag = false;
   } else if (match[0] === "do()") {
